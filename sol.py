43f7634bcae65a8da46036e33554e5e437eedca6
Sweta Sergeeva <sweta.sergeeva@gmail.com>
2024-09-26 15:34:37 +0300

    +Класс для бойца

--- sol.py
+++ sol.py
@@ -15,3 +15,20 @@ class Sword(Weapon):
 class Bow(Weapon):
     def attack(self):
         print("Боец стреляет из лука.")
+
+
+# Класс для бойца
+class Fighter:
+    def __init__(self, name):
+        self.name = name
+        self.weapon = None  # По умолчанию оружие не выбрано
+
+    def changeWeapon(self, weapon: Weapon):
+        self.weapon = weapon
+        print(f"{self.name} выбирает новое оружие.")
+
+    def attack(self):
+        if self.weapon:
+            self.weapon.attack()
+        else:
+            print(f"{self.name} не вооружен!")
